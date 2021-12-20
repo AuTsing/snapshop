8cadf45d8f8c17207b85f684142c61034dd23d74
AuTsing <763025696@qq.com>
2021-12-20 18:43:21 +0800

    add vscode config

---
+++ vite.config.for-vscode.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    plugins: [vue()],
+    build: {
+        outDir: '../../dist/snapshop',
+        target: 'esnext',
+        assetsInlineLimit: 100000000,
+        chunkSizeWarningLimit: 100000000,
+        cssCodeSplit: false,
+        reportCompressedSize: false,
+        rollupOptions: {
+            output: {
+                inlineDynamicImports: true,
+                manualChunks: undefined,
+            },
+        },
+    },
+});
