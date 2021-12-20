d997e951188b83b3fde5d83226c259b0a35c81af
AuTsing <763025696@qq.com>
2021-12-20 17:46:02 +0800

    update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,17 @@ import vue from '@vitejs/plugin-vue';
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [vue()],
+    build: {
+        target: 'esnext',
+        assetsInlineLimit: 100000000,
+        chunkSizeWarningLimit: 100000000,
+        cssCodeSplit: false,
+        reportCompressedSize: false,
+        rollupOptions: {
+            output: {
+                inlineDynamicImports: true,
+                manualChunks: undefined,
+            },
+        },
+    },
 });
