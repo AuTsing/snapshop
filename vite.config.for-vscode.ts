775650c8272b52197d6b9997e6d5be1c22d1a6c1
AuTsing <763025696@qq.com>
2021-12-20 22:44:16 +0800

    update config and delete favicon

--- vite.config.for-vscode.ts
+++ vite.config.for-vscode.ts
@@ -6,6 +6,7 @@ export default defineConfig({
     plugins: [vue()],
     build: {
         outDir: '../../dist/snapshop',
+        emptyOutDir: true,
         target: 'esnext',
         assetsInlineLimit: 100000000,
         chunkSizeWarningLimit: 100000000,
