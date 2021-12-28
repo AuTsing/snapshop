c24619738378c502f20da3549533649323edd6f6
AuTsing <763025696@qq.com>
2021-12-28 23:12:31 +0800

    update config for vscode

--- vite.config.for-vscode.ts
+++ vite.config.for-vscode.ts
@@ -4,7 +4,6 @@ import Components from 'unplugin-vue-components/vite';
 import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers';
 import { getThemeVariables } from 'ant-design-vue/dist/theme';
 
-// https://vitejs.dev/config/
 export default defineConfig({
     plugins: [
         vue(),
@@ -35,6 +34,11 @@ export default defineConfig({
                 }),
                 javascriptEnabled: true,
             },
+        },
+    },
+    resolve: {
+        alias: {
+            'jimp/browser/lib/jimp': 'jimp',
         },
     },
 });
