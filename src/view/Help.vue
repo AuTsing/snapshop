2e5b2d8d2fe0f6fe3d0e7af7287d32aec483eebd
AuTsing <763025696@qq.com>
2023-05-03 03:24:23 +0800

    update: Help page

--- src/view/Help.vue
+++ src/view/Help.vue
@@ -10,7 +10,8 @@ const sampleHotkeys = [
     { key: '4', hotkey: 'D / 右', action: '光标右移 1 像素' },
     { key: '5', hotkey: 'Q', action: '取范围左上' },
     { key: '6', hotkey: 'E', action: '取范围右下' },
-    { key: '7', hotkey: '1-9 / 鼠标左键', action: '取点 1-9' },
+    { key: '7', hotkey: '1-9', action: '取点 1-9' },
+    { key: '7', hotkey: '鼠标左键', action: '取点至 n+1' },
     { key: '8', hotkey: 'Z', action: '清空点列表' },
     { key: '9', hotkey: 'X', action: '清空范围' },
     { key: '10', hotkey: 'C', action: '重新抓取已选点颜色' },
@@ -45,34 +46,44 @@ const keywords = [
 </script>
 
 <template>
-    <a-typography :style="{ margin: '16px' }">
-        <a-typography-title>加载图片</a-typography-title>
-        <a-typography-title :level="2">远程</a-typography-title>
-        <a-typography-paragraph style="width: 70%"> Snapshop 支持从 Http/Https 加载图片。 </a-typography-paragraph>
-        <a-typography-paragraph style="width: 70%">
-            但这需要你自行创建一个后端程序以提供服务，在设置中填入正确的接口地址，就可以从主面板中远程加载图片了。
-        </a-typography-paragraph>
-        <a-typography-paragraph style="width: 70%"> 注意，后端请正确配置 CORS ，否则，这将可能导致无法加载图片。 </a-typography-paragraph>
+    <a-typography :style="{ width: '70%', margin: '16px' }">
+        <a-typography-title :level="2">加载图片</a-typography-title>
+        <a-typography-title :level="4">从远程加载图片</a-typography-title>
+        <a-typography-paragraph> Snapshop 支持从 HTTP / HTTPS / WS 加载图片。 </a-typography-paragraph>
+        <a-typography-paragraph> 这需要你运行一个服务程序以提供图片。 </a-typography-paragraph>
+        <a-typography-title :level="4">从 HTTP(S) 加载图片</a-typography-title>
+        <a-typography-paragraph> Snapshop 会请求 ArrayBuffer 的格式类型，请从接口直接返回图片。 </a-typography-paragraph>
+        <a-typography-paragraph> 请正确配置 CORS ，否则，这将可能导致无法加载图片。 </a-typography-paragraph>
         <a-typography-link href="https://developer.mozilla.org/zh-CN/docs/Web/HTTP/CORS" target="_blank"> 什么是 CORS ？ </a-typography-link>
-        <a-typography-title>采集</a-typography-title>
-        <a-typography-title :level="2">快捷键</a-typography-title>
-        <a-typography-paragraph style="width: 70%"> Snapshop 支持 WASD / 上下左右 两套操作方案。 </a-typography-paragraph>
-        <a-typography-paragraph style="width: 50%">
+        <a-typography-title :level="4">从 WS 加载图片</a-typography-title>
+        <a-typography-paragraph> WS 是更为推荐远程加载图片方式。 </a-typography-paragraph>
+        <a-typography-paragraph> 交互应遵循以下接口： </a-typography-paragraph>
+        <a-typography-paragraph :code="true">
+            interface SnapshotCommand { cmd: 'snapshot'; data: { success: boolean; message: string; file: number[] }; }
+        </a-typography-paragraph>
+        <a-typography-paragraph> Snapshop 截图时会发送： </a-typography-paragraph>
+        <a-typography-paragraph :code="true"> { cmd: 'snapshot'; data: { success: true; message: ""; file: [] }; } </a-typography-paragraph>
+        <a-typography-paragraph> 服务程序应发送以完成交互： </a-typography-paragraph>
+        <a-typography-paragraph :code="true"> { cmd: 'snapshot'; data: { success: true; message: ""; file: [0,0,0,...] }; } </a-typography-paragraph>
+
+        <a-typography-title :level="2">采集</a-typography-title>
+        <a-typography-title :level="4">快捷键</a-typography-title>
+        <a-typography-paragraph> Snapshop 支持 WASD / 上下左右 两套操作方案。 </a-typography-paragraph>
+        <a-typography-paragraph>
             <a-table :columns="sampleHotkeysColumns" :data-source="sampleHotkeys" bordered size="small" :pagination="false"></a-table>
         </a-typography-paragraph>
-        <a-typography-title>生成代码</a-typography-title>
-        <a-typography-title :level="2">快捷键</a-typography-title>
-        <a-typography-paragraph style="width: 70%"> 在主面板可以通过快捷键生成代码。 </a-typography-paragraph>
-        <a-typography-paragraph style="width: 50%">
+
+        <a-typography-title :level="2">生成代码</a-typography-title>
+        <a-typography-title :level="4">快捷键</a-typography-title>
+        <a-typography-paragraph> 在主面板可以通过快捷键生成代码。 </a-typography-paragraph>
+        <a-typography-paragraph>
             <a-table :columns="generateCodeHotkeysColumns" :data-source="generateCodeHotkeys" bordered size="small" :pagination="false"></a-table>
         </a-typography-paragraph>
-        <a-typography-title :level="2">关键字</a-typography-title>
-        <a-typography-paragraph style="width: 70%">
-            在 模板 中填入关键字，生成代码时会自动替换为对应变量，如果有高级需求，请填写正则表达式进行替换。
-        </a-typography-paragraph>
-        <a-typography-paragraph style="width: 70%"> $point[n] 关键字 在 模板 中， n 需替换为对应有效点的序号。 </a-typography-paragraph>
-        <a-typography-paragraph style="width: 70%"> $point[n] 关键字 在 points构成定义 中，无需替换。 </a-typography-paragraph>
-        <a-typography-paragraph style="width: 50%">
+        <a-typography-title :level="4">关键字</a-typography-title>
+        <a-typography-paragraph> 在 模板 中填入关键字，生成代码时会自动替换为对应变量，如果有高级需求，请填写正则表达式进行替换。 </a-typography-paragraph>
+        <a-typography-paragraph> $point[n] 关键字 在 模板 中， n 需替换为对应有效点的序号。 </a-typography-paragraph>
+        <a-typography-paragraph> $point[n] 关键字 在 points构成定义 中，无需替换。 </a-typography-paragraph>
+        <a-typography-paragraph>
             <a-table :columns="keywordsColumns" :data-source="keywords" bordered size="small" :pagination="false"></a-table>
         </a-typography-paragraph>
     </a-typography>
