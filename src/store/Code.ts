58c98828e1f85d012f107406557f3352fee84239
AuTsing <763025696@qq.com>
2023-05-03 18:50:16 +0800

    refactor: CodeGenerator

--- src/store/Code.ts
+++ src/store/Code.ts
@@ -3,6 +3,73 @@ import { useRecordStore } from './Record';
 import { useAreaStore } from './Area';
 import { useDisk } from '../plugins/Disk';
 
+export enum GenerateActions {
+    Text = 'text',
+    Pointx = 'pointx',
+    Pointy = 'pointy',
+    Pointc = 'pointc',
+    Area = 'area',
+    Repeat = 'repeat',
+    Delete = 'delete',
+}
+
+export enum AreaLtrb {
+    Left = 'left',
+    Top = 'top',
+    Right = 'right',
+    Bottom = 'bottom',
+}
+
+export type GenerateStep =
+    | TextGenerateStep
+    | PointxGenerateStep
+    | PointyGenerateStep
+    | PointcGenerateStep
+    | AreaGenerateStep
+    | ReapeatGenerateStep
+    | DeleteGenerateStep;
+
+export interface BaseGenerateStep {
+    action: GenerateStep['action'];
+}
+
+export interface TextGenerateStep extends BaseGenerateStep {
+    action: GenerateActions.Text;
+    text: string;
+}
+
+export interface PointxGenerateStep extends BaseGenerateStep {
+    action: GenerateActions.Pointx;
+    index: number | 'n';
+}
+
+export interface PointyGenerateStep extends BaseGenerateStep {
+    action: GenerateActions.Pointy;
+    index: number | 'n';
+}
+
+export interface PointcGenerateStep extends BaseGenerateStep {
+    action: GenerateActions.Pointc;
+    index: number | 'n';
+}
+
+export interface AreaGenerateStep extends BaseGenerateStep {
+    action: GenerateActions.Area;
+    ltrb: AreaLtrb;
+}
+
+export interface ReapeatGenerateStep extends BaseGenerateStep {
+    action: GenerateActions.Repeat;
+    steps: number;
+    from: number | 'n';
+    to: number | 'n';
+}
+
+export interface DeleteGenerateStep extends BaseGenerateStep {
+    action: GenerateActions.Delete;
+    count: number;
+}
+
 export interface ICodeState {
     template1: string;
     template2: string;
@@ -12,6 +79,7 @@ export interface ICodeState {
     regexp: string;
     regexpReplacement: string;
     pointDefinition: string;
+    flow1: GenerateStep[];
 }
 
 export const defaultCode: ICodeState = {
@@ -23,6 +91,7 @@ export const defaultCode: ICodeState = {
     regexp: '',
     regexpReplacement: '',
     pointDefinition: '{$point[n][x],$point[n][y],$point[n][c]}',
+    flow1: [],
 };
 
 export const useCodeStore = defineStore('code', {
@@ -37,52 +106,104 @@ export const useCodeStore = defineStore('code', {
             regexp: disk.useStorage('regexp', defaultCode.regexp),
             regexpReplacement: disk.useStorage('regexpReplacement', defaultCode.regexpReplacement),
             pointDefinition: disk.useStorage('pointDefinition', defaultCode.pointDefinition),
+            flow1: disk.useStorage('flow1', defaultCode.flow1),
         };
     },
     actions: {
         resetCode() {
             this.$patch(defaultCode);
         },
+        addStep(step: GenerateStep) {
+            this.flow1.push(step);
+        },
+        removeStep(index: number) {
+            this.flow1.splice(index, 1);
+        },
         generate(i: number) {
             const record = useRecordStore();
             const area = useAreaStore();
+            const records = record.records.filter(record => record.cNative !== -1);
+            const flow = this[`flow${i}` as keyof ICodeState] as GenerateStep[];
 
-            const validRecords = record.records.filter(record => record.cNative !== -1);
-            const points = validRecords
-                .map(record =>
-                    this.pointDefinition
-                        .replace(/\$point\[n\]\[x\]/g, record.x.toString())
-                        .replace(/\$point\[n\]\[y\]/g, record.y.toString())
-                        .replace(/\$point\[n\]\[c\]/g, record.c.toString())
-                )
-                .join(',');
-
-            const delta = validRecords
-                .map(record => `${record.x - validRecords[0].x}|${record.y - validRecords[0].y}|${record.c}`)
-                .slice(1)
-                .join(',');
-            const areaJoined = `${area.x1},${area.y1},${area.x2},${area.y2}`;
-
-            const template = this[`template${i}` as keyof ICodeState];
-
-            let code = template
-                .replace(/\$points/g, points)
-                .replace(/\$delta/g, delta)
-                .replace(/\$area/g, areaJoined)
-                .replace(/\$point\[([1-9])\]\[x\]/g, (_str, i) => validRecords[parseInt(i) - 1]?.x.toString() ?? '')
-                .replace(/\$point\[([1-9])\]\[y\]/g, (_str, i) => validRecords[parseInt(i) - 1]?.y.toString() ?? '')
-                .replace(/\$point\[([1-9])\]\[c\]/g, (_str, i) => validRecords[parseInt(i) - 1]?.c.toString() ?? '')
-                .replace(
-                    /\$point\[([1-9])\]/g,
-                    (_str, i) => `${validRecords[parseInt(i) - 1]?.x ?? ''},${validRecords[parseInt(i) - 1]?.y ?? ''},${validRecords[parseInt(i) - 1]?.c ?? ''}`
-                );
-
-            const regexps = this.regexp.match(/^\/(.*)\/([a-z]*)$/);
-            if (regexps && regexps.length > 0) {
-                const regexp = new RegExp(regexps[1], regexps[2]);
-                code = code.replace(regexp, this.regexpReplacement);
+            for (const step of flow) {
+                if (step.action === GenerateActions.Repeat) {
+                    const index = flow.indexOf(step);
+                    const flowSlice = flow.slice(index - step.steps, index);
+                    const from = step.from === 'n' ? records.length : step.from;
+                    const to = step.to === 'n' ? records.length : step.to;
+                    const generatedSlice = [] as GenerateStep[];
+                    for (let i = from; i <= to; i++) {
+                        flowSlice.forEach(step => {
+                            const clone = Object.assign({}, step);
+                            if (clone.action === GenerateActions.Pointx || clone.action === GenerateActions.Pointy || clone.action === GenerateActions.Pointc) {
+                                clone.index = i;
+                            }
+                            generatedSlice.push(clone);
+                        });
+                    }
+                    flow.splice(index - step.steps, flowSlice.length + 1, ...generatedSlice);
+                }
+            }
+            for (const step of flow) {
+                if (step.action === GenerateActions.Delete) {
+                    const index = flow.indexOf(step);
+                    flow.splice(index - step.count, step.count + 1);
+                }
             }
 
+            console.log(flow);
+
+            let code = '';
+            for (const step of flow) {
+                switch (step.action) {
+                    case GenerateActions.Text:
+                        code = code + step.text;
+                        break;
+                    case GenerateActions.Pointx:
+                        if (step.index == 'n') {
+                            code = code + '$p[n][x]';
+                        } else {
+                            code = code + (records[step.index - 1] ? records[step.index - 1].x.toString() : `$p[${step.index}][x]`);
+                        }
+                        break;
+                    case GenerateActions.Pointy:
+                        if (step.index == 'n') {
+                            code = code + '$p[n][x]';
+                        } else {
+                            code = code + (records[step.index - 1] ? records[step.index - 1].y.toString() : `$p[${step.index}][y]`);
+                        }
+                        break;
+                    case GenerateActions.Pointc:
+                        if (step.index == 'n') {
+                            code = code + '$p[n][c]';
+                        } else {
+                            code = code + (records[step.index - 1] ? records[step.index - 1].cNative.toString() : `$p[${step.index}][c]`);
+                        }
+                        break;
+                    case GenerateActions.Area:
+                        switch (step.ltrb) {
+                            case AreaLtrb.Left:
+                                code = code + area.x1.toString();
+                                break;
+                            case AreaLtrb.Top:
+                                code = code + area.y1.toString();
+                                break;
+                            case AreaLtrb.Right:
+                                code = code + area.x2.toString();
+                                break;
+                            case AreaLtrb.Bottom:
+                                code = code + area.y2.toString();
+                                break;
+                            default:
+                                break;
+                        }
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            console.log(code);
             return code;
         },
     },
--- src/view/CodeGenerator.vue
+++ src/view/CodeGenerator.vue
@@ -1,18 +1,104 @@
 <script setup lang="ts">
-import { reactive, toRaw } from 'vue';
-import { useCodeStore } from '../store/Code';
+import { computed, reactive, ref, toRaw } from 'vue';
 import { debouncedWatch } from '@vueuse/core';
 import { message } from 'ant-design-vue';
+
+import { GenerateActions, useCodeStore } from '../store/Code';
 import { defaultCode } from '../store/Code';
-import type { ICodeState } from '../store/Code';
-import ResetButtonVue from '../shared/ResetButton.vue';
+import { AreaLtrb, ICodeState } from '../store/Code';
 import { useControlCv } from '../plugins/ControlCv';
+import ResetButtonVue from '../shared/ResetButton.vue';
 
 const codeStore = useCodeStore();
 const controlCv = useControlCv();
 
 const codeModelRef = reactive<ICodeState>(Object.assign({}, codeStore.$state));
+const addingGenerateAction = ref<GenerateActions>(GenerateActions.Text);
+const addingTextActionText = ref<string>('');
+const addingPointxActionMode = ref<'指定点' | '自动填充点 N'>('自动填充点 N');
+const addingPointxActionIndex = ref<number>(1);
+const addingPointyActionMode = ref<'指定点' | '自动填充点 N'>('自动填充点 N');
+const addingPointyActionIndex = ref<number>(1);
+const addingPointcActionMode = ref<'指定点' | '自动填充点 N'>('自动填充点 N');
+const addingPointcActionIndex = ref<number>(1);
+const addingAreaActionLtrb = ref<AreaLtrb>(AreaLtrb.Left);
+const addingRepeatActionSteps = ref<number>(1);
+const addingRepeatActionFromMode = ref<'指定开始' | '自动填充点列表长度'>('自动填充点列表长度');
+const addingRepeatActionFrom = ref<number>(1);
+const addingRepeatActionToMode = ref<'指定结束' | '自动填充点列表长度'>('自动填充点列表长度');
+const addingRepeatActionTo = ref<number>(1);
+const addingDeleteActionCount = ref<number>(1);
+const flow1 = computed<string[]>(() =>
+    codeModelRef.flow1.map(step => {
+        switch (step.action) {
+            case GenerateActions.Text:
+                return `添加文本: ${step.text}`;
+            case GenerateActions.Pointx:
+                return `添加点 ${step.index} 的 X 值`;
+            case GenerateActions.Pointy:
+                return `添加点 ${step.index} 的 Y 值`;
+            case GenerateActions.Pointc:
+                return `添加点 ${step.index} 的 颜色值`;
+            case GenerateActions.Area:
+                return `添加范围 ${step.ltrb} 的值`;
+            case GenerateActions.Repeat:
+                return `重复以上 ${step.steps} 个步骤 从 ${step.from} 到 ${step.to}`;
+            case GenerateActions.Delete:
+                return `重复完成后 删除以上 ${step.count} 个步骤`;
+        }
+    })
+);
 
+const handleClickAddStep = () => {
+    switch (addingGenerateAction.value) {
+        case GenerateActions.Text:
+            codeStore.addStep({ action: GenerateActions.Text, text: addingTextActionText.value });
+            break;
+        case GenerateActions.Pointx:
+            codeStore.addStep({
+                action: GenerateActions.Pointx,
+                index: addingPointxActionMode.value === '指定点' ? addingPointxActionIndex.value : 'n',
+            });
+            break;
+        case GenerateActions.Pointy:
+            codeStore.addStep({
+                action: GenerateActions.Pointy,
+                index: addingPointyActionMode.value === '指定点' ? addingPointyActionIndex.value : 'n',
+            });
+            break;
+        case GenerateActions.Pointc:
+            codeStore.addStep({
+                action: GenerateActions.Pointc,
+                index: addingPointcActionMode.value === '指定点' ? addingPointcActionIndex.value : 'n',
+            });
+            break;
+        case GenerateActions.Area:
+            codeStore.addStep({
+                action: GenerateActions.Area,
+                ltrb: addingAreaActionLtrb.value,
+            });
+            break;
+        case GenerateActions.Repeat:
+            codeStore.addStep({
+                action: GenerateActions.Repeat,
+                steps: addingRepeatActionSteps.value,
+                from: addingRepeatActionFromMode.value === '指定开始' ? addingRepeatActionFrom.value : 'n',
+                to: addingRepeatActionToMode.value === '指定结束' ? addingRepeatActionTo.value : 'n',
+            });
+            break;
+        case GenerateActions.Delete:
+            codeStore.addStep({
+                action: GenerateActions.Delete,
+                count: addingDeleteActionCount.value,
+            });
+            break;
+        default:
+            break;
+    }
+};
+const handleClickRemoveStep = (i: number) => {
+    codeStore.removeStep(i);
+};
 const handleClickResetCode = () => {
     Object.assign(codeModelRef, defaultCode);
     codeStore.resetCode();
@@ -35,24 +121,94 @@ debouncedWatch(
 
 <template>
     <a-form :style="{ margin: '16px' }" layout="vertical">
-        <a-form-item v-for="i in 5" :label="`模板${i}`">
+        <a-form-item :label="`流程1`">
             <a-row :gutter="8">
                 <a-col :span="12">
-                    <a-input v-model:value="codeModelRef[`template${i}` as keyof ICodeState]" />
+                    <a-steps direction="vertical" size="small" :current="flow1.length">
+                        <a-step v-for="(step, i) in flow1" :title="step" status="process" @click="() => handleClickRemoveStep(i)" />
+                    </a-steps>
                 </a-col>
-                <a-col>
-                    <a-button @click="() => handleClickGenerateCode(i)">生成代码</a-button>
+                <a-col :span="12">
+                    <a-space direction="vertical" style="width: 50%">
+                        <a-select v-model:value="addingGenerateAction">
+                            <a-select-option :value="GenerateActions.Text">文本</a-select-option>
+                            <a-select-option :value="GenerateActions.Pointx">X 坐标</a-select-option>
+                            <a-select-option :value="GenerateActions.Pointy">Y 坐标</a-select-option>
+                            <a-select-option :value="GenerateActions.Pointc">颜色值</a-select-option>
+                            <a-select-option :value="GenerateActions.Area">范围</a-select-option>
+                            <a-select-option :value="GenerateActions.Repeat">重复</a-select-option>
+                            <a-select-option :value="GenerateActions.Delete">删除</a-select-option>
+                        </a-select>
+                        <a-row v-if="addingGenerateAction === GenerateActions.Text">
+                            <a-input v-model:value="addingTextActionText" placeholder="添加文本" allowClear />
+                        </a-row>
+                        <a-row v-if="addingGenerateAction === GenerateActions.Pointx">
+                            <a-input-number v-model:value="addingPointxActionIndex" :min="1" :max="20" :disabled="addingPointxActionMode === '自动填充点 N'">
+                                <template #addonBefore>
+                                    <a-select v-model:value="addingPointxActionMode">
+                                        <a-select-option value="指定点">指定点</a-select-option>
+                                        <a-select-option value="自动填充点 N">自动填充点 N</a-select-option>
+                                    </a-select>
+                                </template>
+                            </a-input-number>
+                        </a-row>
+                        <a-row v-if="addingGenerateAction === GenerateActions.Pointy">
+                            <a-input-number v-model:value="addingPointyActionIndex" :min="1" :max="20" :disabled="addingPointyActionMode === '自动填充点 N'">
+                                <template #addonBefore>
+                                    <a-select v-model:value="addingPointyActionMode">
+                                        <a-select-option value="指定点">指定点</a-select-option>
+                                        <a-select-option value="自动填充点 N">自动填充点 N</a-select-option>
+                                    </a-select>
+                                </template>
+                            </a-input-number>
+                        </a-row>
+                        <a-row v-if="addingGenerateAction === GenerateActions.Pointc">
+                            <a-input-number v-model:value="addingPointcActionIndex" :min="1" :max="20" :disabled="addingPointcActionMode === '自动填充点 N'">
+                                <template #addonBefore>
+                                    <a-select v-model:value="addingPointcActionMode">
+                                        <a-select-option value="指定点">指定点</a-select-option>
+                                        <a-select-option value="自动填充点 N">自动填充点 N</a-select-option>
+                                    </a-select>
+                                </template>
+                            </a-input-number>
+                        </a-row>
+                        <a-row v-if="addingGenerateAction === GenerateActions.Area">
+                            <a-select v-model:value="addingAreaActionLtrb">
+                                <a-select-option :value="AreaLtrb.Left">左</a-select-option>
+                                <a-select-option :value="AreaLtrb.Top">上</a-select-option>
+                                <a-select-option :value="AreaLtrb.Right">右</a-select-option>
+                                <a-select-option :value="AreaLtrb.Bottom">下</a-select-option>
+                            </a-select>
+                        </a-row>
+                        <a-row v-if="addingGenerateAction === GenerateActions.Repeat">
+                            <a-col>
+                                <a-input-number v-model:value="addingRepeatActionSteps" addonBefore="次数" />
+                                <a-input-number v-model:value="addingRepeatActionFrom" :disabled="addingRepeatActionFromMode === '自动填充点列表长度'">
+                                    <template #addonBefore>
+                                        <a-select v-model:value="addingRepeatActionFromMode">
+                                            <a-select-option value="指定开始">指定开始</a-select-option>
+                                            <a-select-option value="自动填充点列表长度">自动填充点列表长度</a-select-option>
+                                        </a-select>
+                                    </template>
+                                </a-input-number>
+                                <a-input-number v-model:value="addingRepeatActionTo" :disabled="addingRepeatActionToMode === '自动填充点列表长度'">
+                                    <template #addonBefore>
+                                        <a-select v-model:value="addingRepeatActionToMode">
+                                            <a-select-option value="指定结束">指定结束</a-select-option>
+                                            <a-select-option value="自动填充点列表长度">自动填充点列表长度</a-select-option>
+                                        </a-select>
+                                    </template>
+                                </a-input-number>
+                            </a-col>
+                        </a-row>
+                        <a-row v-if="addingGenerateAction === GenerateActions.Delete">
+                            <a-input-number v-model:value="addingDeleteActionCount" addonBefore="次数" />
+                        </a-row>
+                        <a-button @click="handleClickAddStep" style="width: 50%">添加步骤</a-button>
+                        <a-button type="primary" @click="() => handleClickGenerateCode(1)" style="width: 50%">生成代码</a-button>
+                    </a-space>
                 </a-col>
             </a-row>
-        </a-form-item>
-        <a-form-item label="正则表达式">
-            <a-input-group compact>
-                <a-input v-model:value="codeModelRef.regexp" style="width: 25%" placeholder="正则表达式" />
-                <a-input v-model:value="codeModelRef.regexpReplacement" style="width: 25%" placeholder="替换为" />
-            </a-input-group>
-        </a-form-item>
-        <a-form-item label="points构成定义">
-            <a-input v-model:value="codeModelRef.pointDefinition" style="width: 50%" />
         </a-form-item>
         <a-form-item>
             <ResetButtonVue :handleClick="handleClickResetCode" />
